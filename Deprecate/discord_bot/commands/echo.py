9ab5c86ee6375991b43cb777a80d2e83c7052a57
Taggy <Tagischeap@Gmail.com>
2024-10-27 06:48:28 -0400

    Removed Deprecate folder and modified gitignore
    Hosting on AWS.

--- Deprecate/discord_bot/commands/echo.py
+++
@@ -1,8 +0,0 @@
-name = 'echo'
-aliases = ['say']
-description = 'Repeat what is said'
-usage = 'Hello.'
-
-async def execute(client, message, args):
-    fArg = message.content[len(message.content.split()[0]):].strip()  # Get the argument after the command
-    await message.channel.send(fArg)
