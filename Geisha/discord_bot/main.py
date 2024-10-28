79d04347ecb3a74d3778b3688b73eae731571cea
Taggy <Tagischeap@Gmail.com>
2024-10-27 21:22:42 -0400

    Cleaned up main.py, added a silly little thing for Halloween
    I might make the thing check for specific date ranges for when to be active.

--- Geisha/discord_bot/main.py
+++ Geisha/discord_bot/main.py
@@ -1,14 +1,18 @@
 import discord
 import os
 import logging
+from colorama import init, Fore, Style  # Style points
 from dotenv import load_dotenv
 from events.on_message import handle_command  # Import the command handler
+from events.on_message_react import react_to_message  # Import the custom reaction handler
 
 # Load environment variables
 load_dotenv(dotenv_path='keys.env')
-
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
+version = "24.10.27"  # Replace with the actual version if needed
+
+init()  # Initialize colorama
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')  # Clean format
 logger = logging.getLogger(__name__)
@@ -21,21 +25,24 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     logger.info('----------------------------------------------------------------')
-    logger.info(f'Logged in as {client.user.name}!')
-    logger.info(f'Application: {client.user.name}')
+    logger.info(Fore.YELLOW + f'Logged in as ' + Fore.GREEN + f'{client.user.name}' + Fore.YELLOW + f' (ID: ' + Fore.CYAN + f'{client.user.id}' + Fore.YELLOW + f')')
+    logger.info(f'Bot is connected to the following guilds:')
+    for guild in client.guilds:
+        logger.info(f' - ' + Fore.CYAN + f'{guild.name}' + Fore.WHITE + f' (ID: ' + Fore.CYAN + f'{guild.id}' + Fore.WHITE + f', Members: {guild.member_count})')
     logger.info('----------------------------------------------------------------')
-    version = "2410.25"  # Replace with the actual version if needed
+    
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'you. ({version})'))
 
 @client.event
 async def on_message(message):
     """Handles incoming messages and delegates command processing."""
     logger.info(f"Message received: {message.content}")  # Log the received message content
-    if message.author == client.user:  # Ignore messages from her
+    if message.author == client.user:  # Ignore messages from the bot itself
         logger.info("Message is from the bot itself; ignoring.")
-        return  # Exit the function if the message is from her
+        return  # Exit the function if the message is from the bot
 
     await handle_command(client, message)  # Call the command handler function
+    await react_to_message(client, message)  # Call the custom reaction handler
 
 # Run the bot
 if DISCORD_TOKEN:
